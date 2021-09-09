d01b54e688ba8f97a60533a0b06a6b7f7619a6f7
Wlad <wladkrychin@gmail.com>
2021-09-09 19:28:31 +0200

    1

--- src/views/MovieDetails.jsx
+++ src/views/MovieDetails.jsx
@@ -9,7 +9,7 @@ import {
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { FetchFilmsById } from "../services/FetchApi";
-import MovieDetailsInfo from "../components/MovieDetailsInfo/MovieDetailsInfo";
+import MovieDetailsInfo from "../components/MovieDetainlsInfo/MovieDetailsInfo";
 import AdditionalInfo from "../components/AdditionalInfo/AdditionalInfo";
 const Cast = React.lazy(() => import("../components/Cast/Cast"));
 const Reviews = React.lazy(() => import("../components/Reviews/Reviews"));
